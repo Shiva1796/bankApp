d23b6bc61bb456013a08dfbf9b713509bc02545a
Mauricio Sibulski <mauricio.sibulski@outlook.com>
2022-05-05 17:43:45 +1200

    Add register page and dashboard with welcome title

--- app.js
+++ app.js
@@ -1,13 +1,24 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const app = express();
 app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
 const port = process.env.PORT || 3000;
 app.use(express.static("public"));
 
 
 app.get('/', (req,res) => {
     res.render("index");
+});
+
+app.post('/', (req,res) => {
+    console.log(req.body);
+    res.render("dashboard", { userName: req.body.username });
+})
+
+app.get('/register', (req,res) => {
+    res.render("register");
 });
 
 app.get('/about', (req,res) => {
