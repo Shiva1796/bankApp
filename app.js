b50497a63ca5e4906c7504040cdcc02ed93f0677
Mauricio Sibulski <mauricio.sibulski@outlook.com>
2022-05-05 15:44:55 +1200

    Create pages for everything and links for them

--- app.js
+++ app.js
@@ -10,6 +10,19 @@ app.get('/', (req,res) => {
     res.render("index");
 });
 
+app.get('/about', (req,res) => {
+    res.render("about");
+});
+
+app.get('/contact', (req,res) => {
+    res.render("contact");
+});
+
+app.get('/testimonials', (req,res) => {
+    res.render("testimonials");
+});
+
+
 app.listen(port, function() {
     console.log("Server started on port 3000");
   });
