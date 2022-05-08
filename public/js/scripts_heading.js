829cc43a17dc688ca77703203fe0198f787b3173
Mauricio Sibulski <mauricio.sibulski@outlook.com>
2022-05-08 14:34:00 +1200

    Separate script files per page

--- public/js/scripts_heading.js
+++ public/js/scripts_heading.js
@@ -1,6 +1,3 @@
-const tabs = document.querySelectorAll(".operations__tab");
-const tabsContainer = document.querySelector(".operations__tab-container");
-const tabsContent = document.querySelectorAll(".operations__content");
 const toggleButton = document.querySelector(".toggle-button");
 const navLinks = document.querySelector(".navbar");
 
@@ -19,24 +16,6 @@ document.addEventListener("click", (e) => {
     if (dropdown === currentDropdown) return;
     dropdown.classList.remove("active");
   });
-});
-tabsContainer.addEventListener("click", function (e) {
-  const clicked = e.target.closest(".operations__tab");
-
-  // Guard clause
-  if (!clicked) return;
-
-  // Remove active classes
-  tabs.forEach((t) => t.classList.remove("operations__tab--active"));
-  tabsContent.forEach((c) => c.classList.remove("operations__content--active"));
-
-  // Activate tab
-  clicked.classList.add("operations__tab--active");
-
-  // Activate content area
-  document
-    .querySelector(`.operations__content--${clicked.dataset.tab}`)
-    .classList.add("operations__content--active");
 });
 
 //Hamburger Menu
