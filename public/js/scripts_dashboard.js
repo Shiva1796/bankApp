376aa596a9de5029eb92a0bbac40d39a46654f4a
Shiva1796 <shiva96chawla@gmail.com>
2022-05-10 20:37:31 -0400

    Added logic to display transactions

--- public/js/scripts_dashboard.js
+++ public/js/scripts_dashboard.js
@@ -1,9 +1,12 @@
 const welcomeMessage = document.getElementById("greeting");
 const balance = document.querySelector(".current-balance");
-const transactions = document.querySelector(".transactions");
+const dashboard = document.querySelector(".dashboard");
+const transactionsContainer = document.querySelector(".transactions-container");
+const movementsContainer = document.querySelector(".movements");
 const inputName = document.querySelector("#login-username");
 const accPin = document.querySelector("#login-password");
 const submit = document.querySelector("#login-submit");
+
 const account1 = {
   name: "Shiva",
   userName: "shiva",
@@ -15,7 +18,7 @@ const account2 = {
   name: "Mauricio",
   userName: "mauricio",
   password: 1234,
-  movements: [200, 450, -400, 3000, -650, -130, 70, 1300, 50],
+  movements: [200, 450, -400, 3000, -650, -130, 70, 1300, 50, 3000],
   interestRate: 1.2,
 };
 const account3 = {
@@ -26,13 +29,7 @@ const account3 = {
 };
 
 const accounts = [account1, account2, account3];
-
-const calcBalance = function (movements) {
-  const currentBalance = movements.reduce((acc, mov) => acc + mov, 0);
-  balance.textContent = `${currentBalance}$`;
-};
-
-calcBalance(account1.movements);
+//The function below is made to calculate the total balance of the account
 
 // const createUsernames = function (accounts) {
 //   accounts.forEach(function (account) {
@@ -57,3 +54,23 @@ submit.addEventListener("click", (e) => {
     }`;
   }
 });
+//Function to display transactions
+const displayTransactions = function (movements) {
+  transactionsContainer.innerHTML = "";
+  movements.forEach(function (mov, i) {
+    const html = `<div class="transaction"> Transaction ${
+      i + 1
+    } : ${mov} $</div>`;
+    transactionsContainer.insertAdjacentHTML("afterbegin", html);
+  });
+};
+
+//Function to calculate balance
+const calcBalance = function (movements) {
+  const currentBalance = movements.reduce((acc, mov) => acc + mov, 0);
+  balance.textContent = `${currentBalance} $`;
+};
+
+//Functions
+calcBalance(account1.movements);
+displayTransactions(account1.movements);
