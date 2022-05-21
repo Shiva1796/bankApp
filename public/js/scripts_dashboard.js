1bb4caa111428716682c9f6c1f5ef3f71f1cf60f
Shiva1796 <shiva96chawla@gmail.com>
2022-05-20 20:34:27 -0400

    Added loan functionality

--- public/js/scripts_dashboard.js
+++ public/js/scripts_dashboard.js
@@ -2,10 +2,14 @@ const welcomeMessage = document.getElementById("greeting");
 const balance = document.querySelector(".current-balance");
 const dashboard = document.querySelector(".dashboard");
 const transactionsContainer = document.querySelector(".transactions-container");
+const transactions = document.querySelectorAll(".transactions");
 const movementsContainer = document.querySelector(".movements");
 const inputName = document.querySelector("#login-username");
 const accPin = document.querySelector("#login-password");
 const submit = document.querySelector("#login-submit");
+const approval = document.querySelector("#submit-btn");
+const amount = document.querySelector("#amount");
+const loanMessage = document.querySelector("#message");
 
 const account1 = {
   name: "Shiva",
@@ -13,7 +17,6 @@ const account1 = {
   password: 1234,
   movements: [
     5000, 3400, -150, -790, -3210, -1000, 8500, -30, 5000, 200, 450, -400, 3000,
-    -650, -130, 70, 1300, 50, 3000,
   ],
   interestRate: 1.5,
 };
@@ -59,6 +62,7 @@ const calcBalance = function (movements) {
 
 let currentAccount;
 //Logic for logging in
+
 submit.addEventListener("click", (e) => {
   e.preventDefault();
   currentAccount = accounts.find((acc) => acc.userName === inputName.value);
@@ -71,5 +75,22 @@ submit.addEventListener("click", (e) => {
     //Using the current account variable whenever a user logs in their current balance and transactions will be shown
     calcBalance(currentAccount.movements);
     displayTransactions(currentAccount.movements);
+    loanApproval(currentAccount.movements);
   }
 });
+const loanApproval = function (movements) {
+  const currentBalance = movements.reduce((acc, mov) => acc + mov, 0);
+  approval.addEventListener("click", function (e) {
+    e.preventDefault();
+    if (amount.value >= 1000 && amount.value < 2 * currentBalance) {
+      return (loanMessage.textContent =
+        "Your loan will be reviewed and you will be contacted shortly!");
+    } else if (amount.value < 1000) {
+      return (loanMessage.textContent =
+        "Sorry but we do not loan less than $1000");
+    } else {
+      return (loanMessage.textContent =
+        "Sorry but we do not loan more than twice the account balance");
+    }
+  });
+};
